23c75b6a9acacf478db366be53a697e74e24c287
Lakshmibabu Nirmala <lakshmibabu.nirmala@hrblock.com>
2022-07-06 11:41:03 +0530

    Added form controls with validations in premium calc form

--- TAL.Client/src/app/app.module.ts
+++ TAL.Client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PermiumCalcFormComponent } from './components/permium-calc-form/permium-calc-form.component';
 import { PermiumCalcResultComponent } from './components/permium-calc-result/permium-calc-result.component';
@@ -10,7 +11,7 @@ import { PermiumCalcResultComponent } from './components/permium-calc-result/per
     PermiumCalcFormComponent,
     PermiumCalcResultComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- TAL.Client/src/app/components/permium-calc-form/permium-calc-form.component.ts
+++ TAL.Client/src/app/components/permium-calc-form/permium-calc-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-permium-calc-form',
@@ -6,5 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./permium-calc-form.component.scss'],
 })
 export class PermiumCalcFormComponent implements OnInit {
-  ngOnInit(): void {}
+  public premiumForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.generateForm();
+  }
+
+  // Create Form
+  generateForm() {
+    this.premiumForm = this.formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(100),
+        ],
+      ],
+      dob: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+          ),
+        ],
+      ],
+      age: ['', Validators.required],
+      occupation: ['', Validators.required],
+      deathCoverageAmount: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^\d+(\.\d{1,2})?$/),
+          Validators.min(1),
+        ],
+      ],
+    });
+  }
 }
